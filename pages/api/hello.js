62c7f0bf76c74ef63b811d441485337314393391
Anant Jain <110173387+anant-jain2011@users.noreply.github.com>
2026-05-28 21:23:41 +0530

    Delete pages/api/hello.js

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-import cook from "middleware/mogod";
-import mongoose from "mongoose";
-
-export default async function handler(req, res) {
-  await cook();
-  const newItem = new StringModel({ text: req.body.text });
-  await newItem.save();
-  res.status(200).json({ newItem });
-}
