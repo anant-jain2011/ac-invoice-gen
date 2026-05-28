1f59ca7579ede18a8e41aa27e46f80c9c014ec6e
Anant Jain <110173387+anant-jain2011@users.noreply.github.com>
2026-05-28 21:20:25 +0530

    Refactor cook function in mogod.js
    Refactor cook function to remove async keyword and use an IIFE.

--- middleware/mogod.js
+++ middleware/mogod.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
-export default async function cook() {
+export default function cook() {
+  (async () => {
   await mongoose
     .connect(process.env.MONGODB_URI + "ACInvoice")
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
+  })()
 }
