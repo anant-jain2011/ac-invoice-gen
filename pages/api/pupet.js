04a822534b736a02fa51fbebeed4584af4cb0469
Anant Jain <110173387+anant-jain2011@users.noreply.github.com>
2026-05-29 14:28:06 +0530

    Update pupet.js

--- pages/api/pupet.js
+++ pages/api/pupet.js
@@ -12,7 +12,6 @@ export default async function handler(req, res) {
 
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath: puppeteer.executablePath(),
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
